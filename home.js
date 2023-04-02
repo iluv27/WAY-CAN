f4a320b69c080ecba1aa197b7c7047c95d1f2aa2
iluv27 <ehiedujoy277@gmail.com>
2023-04-02 03:34:49 +0100

    last commit

--- home.js
+++ home.js
@@ -13,3 +13,27 @@ function scrollFunction() {
         document.querySelector(".started-button").classList.remove("fixed");
     }
 }
+
+
+// For the responsive nav bar
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".pages");
+const navLinks = document.querySelectorAll(".pages li");
+
+burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = "";
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+
+    //Burger Animation
+    burger.classList.toggle("toggle");
+});
+
